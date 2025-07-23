4460d527c0eee8e08240ec436ffd6ffa2262ba80
Saidines <88153926+Saidines@users.noreply.github.com>
2025-07-24 01:51:14 +0530

    Delete src directory

--- src/model/user.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose"
-
-const userschema = new mongoose.Schema({
-	username:{ type:String,required:true,unique:true},
-	password:{ type: String, required: true},});
-	export const user= mongoose.model('user',userschema);
-
